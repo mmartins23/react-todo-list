37ea55bb93ee79510912bff0a3b2156f21841d54
Marlon Martins <marlon-martins@outlook.com>
2023-11-13 16:03:48 +0100

    25. Using the Context from Child Components

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,37 +1,19 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { TodosContext } from "../TodosContext.js";
 import Todo from "./Todo.jsx";
 
-const initialTodos = [
-  {
-    id: 0,
-    title: "Do Groceries",
-    description: "Buy apples, rice and water",
-    isDone: false,
-  },
-  {
-    id: 1,
-    title: "Study React",
-    description: "Understand context and reducers",
-    isDone: false,
-  },
-  {
-    id: 2,
-    title: "Learn Redux",
-    description: "Learn state management with Redux",
-    isDone: false,
-  },
-];
+
 function TodosList() {
-  const [todos, setTodos] = useState(initialTodos);
+  const store = useContext(TodosContext);
 
   function deleteHandler(id) {
     if(confirm('Are you sure you want to delete the to-do?')) {
-      setTodos(todos.filter(todo => todo.id !== id));
+      store.setTodos(store.todos.filter(todo => todo.id !== id));
     }
   }
 
   function toggleIsDoneHandler(id) {
-    setTodos(todos.map(todo => {
+    store.setTodos(store.todos.map(todo => {
       if(todo.id === id) {
         todo.isDone = !todo.isDone;
         return todo;
@@ -45,7 +27,7 @@ function TodosList() {
   return (
     <>
       <div className="todos">
-        {todos.map((todo) => (
+        {store.todos.map((todo) => (
           <Todo 
           todo={todo} 
           key={todo.id}
