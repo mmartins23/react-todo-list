c7cc503548819940cc9227f1bb1283381035af6e
Marlon Martins <marlon-martins@outlook.com>
2023-11-13 15:36:46 +0100

    24. Creating and Providing the To-Dos Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
 import Header from './components/Header.jsx';
 import Home from './views/Home.jsx';
+import { TodosContext } from './TodosContext.js';
+import { useState } from 'react';
+
 import './App.scss';
 
+const initialTodos = [
+  {
+    id: 0,
+    title: "Do Groceries",
+    description: "Buy apples, rice and water",
+    isDone: false,
+  },
+  {
+    id: 1,
+    title: "Study React",
+    description: "Understand context and reducers",
+    isDone: false,
+  },
+  {
+    id: 2,
+    title: "Learn Redux",
+    description: "Learn state management with Redux",
+    isDone: false,
+  },
+];
+
+
+
 function App() {
+
+  const [todos, setTodos] = useState(initialTodos);
 
   return (
     <>
       <main>
-        
+        <TodosContext.Provider value={{todos, setTodos}}/>
         <Header appName="To-Do List with React" />
 
         <Home />
