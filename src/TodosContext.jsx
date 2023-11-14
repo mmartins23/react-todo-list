5ce5437771d6273aa3818bb9b801a10a91cc4b83
Marlon Martins <marlon-martins@outlook.com>
2023-11-14 16:34:47 +0100

    38. Showing The Filtered Todos

--- src/TodosContext.jsx
+++ src/TodosContext.jsx
@@ -22,27 +22,37 @@ const initialTodos = [
   },
 ];
 
-export function TodosProvider({children}) {
+export function TodosProvider({ children }) {
   const [todos, dispatch] = useReducer(todosReducer, initialTodos);
 
   const [modalIsActive, setModalIsActive] = useState(false);
 
   const [filterBy, setFilterBy] = useState("");
-  
 
+  function filteredTodos() {
+    switch (filterBy) {
+      case "todo":
+        return todos.filter((todo) => !todo.isDone);
+      case "done":
+        return todos.filter((todo) => todo.isDone);
+       default:
+        return todos;
+    }
+  }
 
   return (
     <>
       <main>
-        <TodosContext.Provider 
-        value={{ 
-            todos, 
+        <TodosContext.Provider
+          value={{
+            todos,
             dispatch,
-            modalIsActive, 
-            setModalIsActive, 
+            modalIsActive,
+            setModalIsActive,
             filterBy,
-            setFilterBy
-         }}
+            setFilterBy,
+            filteredTodos
+          }}
         >
           {children}
         </TodosContext.Provider>
@@ -51,37 +61,37 @@ export function TodosProvider({children}) {
   );
 }
 
-export function useTodos()  {
-    return useContext(TodosContext)
+export function useTodos() {
+  return useContext(TodosContext);
 }
 
-function todosReducer(todos,action){
-
-    switch (action.type) {
-        case 'deleted': {
-            if(confirm('Are you sure you want to delete the to-do?')){
-                return todos.filter(todo => todo.id !== action.id);
-            } else {
-                return todos;
-            }
-        }
-
-        case 'added': {
-            let newTodo = action.newTodo;
-            newTodo.id = todos.length ? Math.max(...todos.map(todo => todo.id)) + 1: 1;
-            return [...todos, newTodo];
-        }
-
-        case 'toggledIsDone': {
-            return (todos.map(todo => {
-                if (todo.id === action.id) {
-                  todo.isDone = !todo.isDone;
-                  return todo;
-                } else {
-                  return todo;
-                }
-            }));
-        }
+function todosReducer(todos, action) {
+  switch (action.type) {
+    case "deleted": {
+      if (confirm("Are you sure you want to delete the to-do?")) {
+        return todos.filter((todo) => todo.id !== action.id);
+      } else {
+        return todos;
+      }
     }
 
+    case "added": {
+      let newTodo = action.newTodo;
+      newTodo.id = todos.length
+        ? Math.max(...todos.map((todo) => todo.id)) + 1
+        : 1;
+      return [...todos, newTodo];
+    }
+
+    case "toggledIsDone": {
+      return todos.map((todo) => {
+        if (todo.id === action.id) {
+          todo.isDone = !todo.isDone;
+          return todo;
+        } else {
+          return todo;
+        }
+      });
+    }
+  }
 }
